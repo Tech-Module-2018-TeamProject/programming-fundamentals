114d628ff842594bd56ef3b0e3162e93bf3dbcfe
yonkov <yonkov.atanas@gmail.com>
2018-03-19 00:08:28 +0200

    Update Task_2_RectangleAarea.cs

--- intro-and-basic-syntax/execise/Task_2_RectangleAarea.cs
+++ intro-and-basic-syntax/execise/Task_2_RectangleAarea.cs
@@ -7,8 +7,17 @@ namespace SUApp1
 	{
 		static void Main(string[] args)
 		{
+			/*
+			-------------------------------------------------------------------------------------------------------
+			In this task, we need to estimate the area of a rectangle. We are using TryParse method to parse an 
+			integer from the console. This way we can make our custom error handling.
+			--------------------------------------------------------------------------------------------------------
+			*/
+			//take the height of the rectangle
 			int.TryParse(Console.ReadLine(), out int Height);
+			//take the width of the rectangle
 			int.TryParse(Console.ReadLine(), out int Width);
+			//print the result
 			Console.WriteLine( Height * Width);
 		}
 	}
