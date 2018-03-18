e1f2451d4089d849e098095cc51a481a7c3a28f4
cosebose <37365612+cosebose@users.noreply.github.com>
2018-03-18 21:21:22 +0200

    Update Task_1_DebitCardNumber.cs

--- intro-and-basic-syntax/execise/Task_1_DebitCardNumber.cs
+++ intro-and-basic-syntax/execise/Task_1_DebitCardNumber.cs
@@ -7,11 +7,14 @@ namespace _01_Debit_card
     {
         static void Main(string[] args)
         {
+            // we recieve from the console each one of the four card number parts
             int redica1 = int.Parse(Console.ReadLine());
             int redica2 = int.Parse(Console.ReadLine());
             int redica3 = int.Parse(Console.ReadLine());
             int redica4 = int.Parse(Console.ReadLine());
-
+            
+            
+            // we print the complete card number in a 4er placeholders for each part number. 
             Console.Write("{0:d4} ", int1);
             Console.Write("{0:d4} ", int2);
             Console.Write("{0:d4} ", int3);
